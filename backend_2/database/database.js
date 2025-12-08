aee96469253906e896bca7163f92193ecd016636
sillysilvy <silvyespindola2004@gmail.com>
2025-12-07 21:03:44 -0300

    adição do uso de .env para facilitar o uso dos outros membros da equipe

--- backend_2/database/database.js
+++ backend_2/database/database.js
@@ -1,9 +1,16 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const connection = new Sequelize('','root','',{
-    host: 'localhost',
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    timezone: '-03:00'
-});
+    timezone: process.env.DB_TIMEZONE || '-03:00',
+    logging: process.env.NODE_ENV === 'development' ? console.log : false
+  }
+);
 
 module.exports = connection;
--- backend_2/index.js
+++ backend_2/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
@@ -122,6 +123,8 @@ connection.authenticate().then(() => {
     console.log(errorMsg);
 });
 
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
     console.log("server ta on");
 });
